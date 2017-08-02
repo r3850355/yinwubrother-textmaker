b63c13dfc2d3ca4d00b7ab9af68b5ee4d4937ba3
Ren Zhou <r3850355@gmail.com>
2017-08-02 14:19:31 +0800

    修正英文數字不能置中
    *暫時先把英數轉全形當修正

--- js/script.js
+++ js/script.js
@@ -40,7 +40,7 @@
 				
 
 				ctx.drawImage(img,0,0,w,h);
-				ctx.font = "80px  Microsoft YaHei";
+				//ctx.font = "80px  Microsoft YaHei";
 				ctx.fillStyle = "rgba(252,255,255,1)";
 
 
@@ -58,15 +58,22 @@
 				
 				var text_w,text_h,text_l,text_fs
 				text_l = addtext.length; //輸入長度
-				text_fs = w/(text_l+2)  //字體大小修正
+				text_fs = w/(text_l+2);  //字體大小修正
 				text_h = h*0.95;		//離圖片底部的高度
-				text_w = (w/2)- (text_l/2)*text_fs; //文字置中
+				//text_w = (w/2)- (text_l/2)*text_fs; //文字置中
+				text_w = (w-text_l*text_fs)/2;//更正確的算法
+				addtext = Half2Full(addtext);//半形轉全形
+				
+
+				
+				
+
 				
 				
 
 
 				ctx.font = text_fs+"px  Microsoft YaHei";//即時修正字體大小
-				ctx.fillText(addtext,text_w,text_h); //選擇位置
+				ctx.fillText(addtext,text_w,text_h); //選擇位置 && 上字
 				
 				dl_link = canvas.toDataURL();
 				document.getElementById("DL").href=dl_link;
@@ -77,4 +84,24 @@
 		
 		document.getElementById("errBtn").onclick = function(){
 		shuiyin('output',"./img/"+ img_ori +".jpg");	
+		}
+		
+	
+
+	
+		function Half2Full(zStr1) {
+			var i = 0;
+			var aTmp = new Array();
+			var zStr2 = "";
+			for(i = 0; i < zStr1.length; i++) {
+				if(zStr1.charCodeAt(i) >=0 && zStr1.charCodeAt(i) <= 32) {
+					aTmp[i] = 0;   //ascii 小於等於 32的字元，都先清為 null
+				} else if(zStr1.charCodeAt(i) >=33 && zStr1.charCodeAt(i) <= 126)  {
+					aTmp[i] = zStr1.charCodeAt(i) + 65248;   //ascii介於33~126之間的字元，加上65248準備轉為全型unicode
+				} else {
+					aTmp[i] = zStr1.charCodeAt(i);
+				}
+				zStr2 += String.fromCharCode(aTmp[i]);    //轉為全型unicode
+			}
+			return zStr2;  //裝轉換結果回填到原來源字串位置
 		}
